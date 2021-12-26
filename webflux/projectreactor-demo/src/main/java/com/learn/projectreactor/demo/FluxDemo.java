1b63afd64b8fd92d7c08a2a62039de4fe4900531
linbingxing <541079692@qq.com>
2021-12-26 14:48:39 +0800

    初始化项目

---
+++ webflux/projectreactor-demo/src/main/java/com/learn/projectreactor/demo/FluxDemo.java
@@ -0,0 +1,15 @@
+package com.learn.projectreactor.demo;
+
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.ArrayList;
+
+public class FluxDemo {
+
+    public static void main(String[] args) {
+        Flux.range(1,100).subscribe(System.out::println);
+
+
+    }
+}

4ff852f0b3c31fa30c11c989a454677deaaee6a8
linbingxing <541079692@qq.com>
2022-01-01 00:44:00 +0800

    初始化项目

--- webflux/projectreactor-demo/src/main/java/com/learn/projectreactor/demo/FluxOperationDemo.java
+++ webflux/projectreactor-demo/src/main/java/com/learn/projectreactor/demo/FluxOperationDemo.java
@@ -108,7 +108,7 @@ public class FluxOperationDemo {
 
 
     @Test
-    public void zip(){
+    public void zip() {
         Flux flux1 = Flux.just(1, 2);
         Flux flux2 = Flux.just(3, 4);
         Flux.zip(flux1, flux2).subscribe(System.out::println);
@@ -121,4 +121,92 @@ public class FluxOperationDemo {
                 .subscribe(System.out::println);
 
     }
+
+    @Test
+    public void defaultIfEmpty() {
+        Flux.just(null).defaultIfEmpty(33).subscribe(System.out::println);
+    }
+
+    @Test
+    public void takeUntil() {
+        Flux.range(1, 100).takeUntil(i -> i == 10).subscribe(System.out::println);
+        System.out.println("takeWhile");
+        Flux.range(1, 100).takeWhile(i -> i <= 10)
+                .subscribe(System.out::println);
+        System.out.println("bufferUntil");
+        Flux.range(1, 10).bufferUntil(i -> i % 2 == 0)
+                .subscribe(System.out::println);
+        System.out.println("bufferWhile");
+        Flux.range(1, 10).bufferWhile(i -> i % 2 == 0)
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void skipUntil() {
+        Flux.range(1, 100).skipUntil(i -> i == 10).subscribe(System.out::println);
+        System.out.println("skipWhile");
+        Flux.range(1, 100).skipWhile(i -> i < 10).subscribe(System.out::println);
+    }
+
+    @Test
+    public void any() {
+        Flux.just(3, 5, 7, 9, 11, 15, 16, 17)
+                .any(e -> e % 2 == 0)
+                .subscribe(isExisted -> System.out.println(isExisted));
+
+        Flux.just("abc", "ela", "ade", "pqa", "kang")
+                .all(a -> a.contains("a"))
+                .subscribe(isAllContained -> System.out.println(isAllContained));
+
+    }
+
+    @Test
+    public void concat() {
+        Flux.concat(
+                Flux.range(1, 3),
+                Flux.range(4, 2),
+                Flux.range(6, 5)
+        ).subscribe(System.out::println);
+    }
+
+    @Test
+    public void reduce() {
+        Flux.range(1, 10).reduce((x, y) -> x + y)
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void reduceWith() {
+        Flux.range(1, 10).reduceWith(() -> 5, (x, y) -> x + y)
+                .subscribe(System.out::println);
+
+    }
+
+    @Test
+    public void log() {
+        Flux.just(1, 2).log().subscribe(System.out::println);
+    }
+
+    @Test
+    public void debug() {
+        Mono.just(0).map(x -> 1 / x)
+                .checkpoint("debug").subscribe(System.out::println);
+    }
+
+    @Test
+    public void test() {
+        // range操作符创建从[1到100]的整数序列
+        Flux.range(1, 100)
+                // repeat操作符在源流完成之后一次又一次地订阅源响应式流。
+                // repeat操作符订阅流操作符的结果、接收从1到100的元素以及onComplete信号，
+                // 然后再次订阅、接收，不断重复该过程
+                .repeat().
+                // 使用collectList操作符尝试将所有生成的元素收集到一个集合中。
+                // 由于是无限流，最终它会消耗所有内存，导致OOM。
+                collectList().
+                // block操作符触发实际订阅并阻塞正在运行的线程，直到最终结果到达
+                // 当前场景不会发生，因为响应流是无限的。
+                block();
+    }
+
 }

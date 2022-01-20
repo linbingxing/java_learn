8850bda27c46aa93cb4a636d819f7e4f748d6d03
linbingxing <linbingxing@zuipin.cn>
2022-01-20 15:23:37 +0800

    'dubbo'

---
+++ dubbo/dubbo-spring-boot/api-demo/src/main/java/org/learn/dubbo/HelloService.java
@@ -0,0 +1,10 @@
+package org.learn.dubbo;
+
+/**
+  * @description TODO
+  * @author lbx
+  * @date 2022/1/20 12:38
+  * @version 1.0.0
+ **/
+public class HelloService {
+}
---
+++ dubbo/dubbo-spring-boot/provider-demo/src/main/java/org/learn/dubbo/service/HelloServiceImpl.java
@@ -0,0 +1,10 @@
+package org.learn.dubbo.service;
+
+/**
+  * @description TODO
+  * @author lbx
+  * @date 2022/1/20 12:49
+  * @version 1.0.0
+ **/
+public class HelloServiceImpl {
+}
